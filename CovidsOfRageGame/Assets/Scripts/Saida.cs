da2e0750a793b3d6b7e3fd65b22251e6c128eeac
MatheusFama <matheusfamaa@gmail.com>
2021-08-14 19:44:09 -0300

    Fase 2 e Implementacao da proxima fase

--- CovidsOfRageGame/Assets/Scripts/Saida.cs
+++ CovidsOfRageGame/Assets/Scripts/Saida.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,23 @@ using UnityEngine.SceneManagement;
 
 public class Saida : MonoBehaviour
 {
+    public enum Fases
+    {
+        Fase1 = 0,
+        Fase1_Boss,
+        Fase2,
+        Fase2_Boss
+    }
 
+
+    private Fases[] fases = new Fases[] { 
+        Fases.Fase1 
+        ,Fases.Fase1_Boss
+        ,Fases.Fase2
+        ,Fases.Fase2_Boss
+    };
     public GameManager _gm;
+    public Fases fase;
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +42,7 @@ public class Saida : MonoBehaviour
         {
             if(_gm.totalVitimas == _gm.vitimasSalvas)
             {
-                SceneManager.LoadScene("Fase2");
+                SceneManager.LoadScene(Enum.GetName(typeof(Fases),this.fases[((int)this.fase)+ 1])) ;
             }
         }
     }
